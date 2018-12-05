801e0918f6b630a5c4b894f462b254a303d6eeda
Unknown <nicolas_navas_94@hotmail.com>
2018-12-05 09:27:22 +0100

    Proyecto creado

---
+++ app/src/main/java/es/iessaldillo/nicol/nng_pr07_fragmentos/ui/userlist/UserListViewModel.java
@@ -0,0 +1,7 @@
+package es.iessaldillo.nicol.nng_pr07_fragmentos.ui.userlist;
+
+import android.arch.lifecycle.ViewModel;
+
+public class UserListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
